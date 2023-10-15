abc417175b0a997a6868c29ec5cc020d662c4149
Fahmida Faiza <fahmidafaiza57@gmail.com>
2023-10-15 21:57:36 +0600

    added update put

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,6 +31,72 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+//mongodb
+const coffeeCollection = client.db('coffeeDB').collection('coffee');
+
+//get
+app.get('/coffee', async (req, res)=>{
+  const cursor = coffeeCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
+//update
+app.get('/coffee/:id', async(req, res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id) }
+  const result = await coffeeCollection.findOne(query);
+  res.send(result);
+})
+
+
+//post//create
+app.post('/coffee', async(req,res)=>{
+  const newCoffee = req.body;
+  console.log(newCoffee);
+  const result = await coffeeCollection.insertOne(newCoffee);
+  res.send(result)
+})
+
+//put//update
+app.put('/coffee/:id', async(req, res)=>{
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)}
+ const options = {upset: true };
+ const updatedCoffee = req.body;
+ const coffee ={
+
+$set: {
+  name: updatedCoffee.name,
+  quantity: updatedCoffee.quantity,
+  supplier: updatedCoffee.supplier,
+  taste: updatedCoffee.taste,
+  category: updatedCoffee.category,
+ details: updatedCoffee.details,
+  photo: updatedCoffee.photo,
+
+}
+
+
+
+
+
+
+
+
+ } 
+
+ const result = await coffeeCollection.updateOne(filter, coffee, options);
+ res.send(result)
+})
+
+
+//delete
+app.delete('/coffee/:id', async(req,res)=>{
+  const id = req.params.id;
+  const query ={_id: new ObjectId(id) }
+  const result = await coffeeCollection.deleteOne(query);
+  res.send(result);
+})
 
 
 
@@ -51,8 +117,6 @@ async function run() {
 
 
 
-
-    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
